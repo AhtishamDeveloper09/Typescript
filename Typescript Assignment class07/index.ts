05cedf35dea87e9ebc7d0ff9e8f57900b466bc35
AhtishamDeveloper09 <Ahtishamprine@gmail.com>
2023-09-23 23:41:33 +0500

    class07 file updated

--- Typescript Assignment class07/index.ts
+++ Typescript Assignment class07/index.ts
@@ -1,30 +1,30 @@
 // 1- Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
-let n: number= 20;
-let sum: number= 0;
-for(let i= 0; i < n; i++){
+let n: number = 20;
+let sum: number = 0;
+for (let i = 0; i < n; i++) {
     sum += 2 * i;
 }
 console.log("The sum of first n even numbers is:", sum);
 console.log();
 
 // 2- Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
-let arrayOfNumbers: number[]= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arrayOfNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log("The even numbers are:");
-for(let i= 0; i < arrayOfNumbers.length; i++){
-    if(arrayOfNumbers[i] % 2 == 0){
+for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (arrayOfNumbers[i] % 2 == 0) {
         console.log(arrayOfNumbers[i]);
     }
 }
 console.log();
 
 // 3- Implement a program that uses a loop to iterate through an array of numbers and remove all the even numbers from them and just leave the odd ones
-let arrayOfNumbers2: number[]= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arrayOfNumbers2: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function removeEvenNumbers(arrayOfNumbers2: number[]){
-    for(let i= 0; i < arrayOfNumbers2.length; i++){
-        if(arrayOfNumbers2[i] % 2 == 0){
-        arrayOfNumbers2.splice(i, 1);
-        i--;
+function removeEvenNumbers(arrayOfNumbers2: number[]) {
+    for (let i = 0; i < arrayOfNumbers2.length; i++) {
+        if (arrayOfNumbers2[i] % 2 == 0) {
+            arrayOfNumbers2.splice(i, 1);
+            i--;
         }
     }
     return arrayOfNumbers2;
@@ -33,22 +33,22 @@ console.log("Array after removing even numbers is:", removeEvenNumbers(arrayOfNu
 console.log();
 
 // 4- Write a program that defines a function to calculate the area of a circle. The function should take the radius as input and return the calculated area.
-function areaOfCircle(radiusOfCircle: number){
-    let calcualateArea: number= Math.PI * radiusOfCircle **2;
+function areaOfCircle(radiusOfCircle: number) {
+    let calcualateArea: number = Math.PI * radiusOfCircle ** 2;
     return Math.round(calcualateArea);
 }
-let radiusOfCircle: number= 20;
+let radiusOfCircle: number = 20;
 console.log("The area of circle is:", areaOfCircle(radiusOfCircle));
 console.log();
 
 // 4- Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
-let listOfGrades: number[]= [60, 45, 75, 53, 35, 48, 77, 28, 84, 96];
+let listOfGrades: number[] = [60, 45, 75, 53, 35, 48, 77, 28, 84, 96];
 
-function removeFailingGrades(listOfGrades: number[]){
-    for(let i= 0; i < arrayOfNumbers2.length; i++){
-        if(listOfGrades[i] < 50){
-        listOfGrades.splice(i, 1);
-        i--;
+function removeFailingGrades(listOfGrades: number[]) {
+    for (let i = 0; i < arrayOfNumbers2.length; i++) {
+        if (listOfGrades[i] < 50) {
+            listOfGrades.splice(i, 1);
+            i--;
         }
     }
     return listOfGrades;
@@ -57,13 +57,13 @@ console.log("After removing failing grades from the array:", removeFailingGrades
 console.log();
 
 // 6- Write a program that uses a function to find the largest element in an array of numbers.
-let arrayOfNumbers3: number[]= [60, 45, 75, 53, 35, 48, 77, 28, 84, 96];
+let arrayOfNumbers3: number[] = [60, 45, 75, 53, 35, 48, 77, 28, 84, 96];
 
-function findLargestElement(arrayOfNumbers3: number[]){
-    let largest: number= arrayOfNumbers3[0];
-    for(let i= 1; i < arrayOfNumbers3.length; i++){
-        if(arrayOfNumbers3[i] > largest){
-            largest= arrayOfNumbers3[i];
+function findLargestElement(arrayOfNumbers3: number[]) {
+    let largest: number = arrayOfNumbers3[0];
+    for (let i = 1; i < arrayOfNumbers3.length; i++) {
+        if (arrayOfNumbers3[i] > largest) {
+            largest = arrayOfNumbers3[i];
         }
     }
     return largest;
