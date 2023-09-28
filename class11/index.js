6313048919a5a7ff012764076505060162561d97
AhtishamDeveloper09 <Ahtishamprine@gmail.com>
2023-09-28 23:41:20 +0500

    class11 file updated

--- class11/index.js
+++ class11/index.js
@@ -1,19 +1,113 @@
 import inquirer from "inquirer";
-async function userInput() {
-    const questions = [
-        {
+/*async function userInput() {
+  const questions: QuestionCollection = [
+    {
+      name: "num1",
+      type: "number",
+      message: "Enter the first number:",
+    },
+    {
+      name: "num2",
+      type: "number",
+      message: "Enter the second number:",
+    }
+  ]
+  const result: results =  await inquirer.prompt(questions)
+  console.log("Result of sum is:", result.num1 + result.num2);
+}
+await userInput()
+console.log("After executing the function.");
+
+// --------------------------------------------------
+
+const questions: QuestionCollection = [{
+    name: "num1",
+    type: "number",
+    message: "Enter first number:"
+},
+{
+    name: "num2",
+    type: "number",
+    message: "Enter second number:"
+},
+{
+    name: "operation",
+    type: "list",
+    message: "Enter the operation:",
+    choices: ["+", "-", "*", "%", "/", "^"]
+}
+]
+let results: Promise<results> = inquirer.prompt(questions);
+results
+    .then((result: results) => {
+        switch (result.operation) {
+            case "+":
+                console.log(`Sum of ${result.num1} + ${result.num2} = ${result.num1 + result.num2}`);
+                break;
+            case "-":
+                console.log(`Subtarction of ${result.num1} - ${result.num2} = ${result.num1 - result.num2}`);
+                break;
+            case "*":
+                console.log(`Multiplication of ${result.num1} * ${result.num2} = ${result.num1 * result.num2}`);
+                break;
+            case "%":
+                console.log(`Modulus of ${result.num1} % ${result.num2} = ${result.num1 % result.num2}`);
+                break;
+            case "/":
+                console.log(`Division of ${result.num1} / ${result.num2} = ${result.num1 / result.num2}`);
+                break;
+            case "^":
+                console.log(`Exponent of ${result.num1} ^ ${result.num2} = ${result.num1 ** result.num2}`);
+                break;
+            default:
+                break;
+        }
+    })
+    .catch((error: results) => {
+        console.error("Something went wrong", error);
+
+    });*/
+async function calculator() {
+    const questions = [{
             name: "num1",
             type: "number",
-            message: "Enter the first number:",
+            message: "Enter first number:"
         },
         {
             name: "num2",
             type: "number",
-            message: "Enter the second number:",
+            message: "Enter second number:"
+        },
+        {
+            name: "operation",
+            type: "list",
+            message: "Enter the operation:",
+            choices: ["+", "-", "*", "%", "/", "^"]
         }
     ];
-    const result = await inquirer.prompt(questions);
-    console.log("Result of sum is:", result.num1 + result.num2);
+    const result = inquirer.prompt(questions);
+    switch (result.operation) {
+        case "+":
+            console.log(`Sum of ${result.num1} + ${result.num2} = ${result.num1 + result.num2}`);
+            break;
+        case "-":
+            console.log(`Subtarction of ${result.num1} - ${result.num2} = ${result.num1 - result.num2}`);
+            break;
+        case "*":
+            console.log(`Multiplication of ${result.num1} * ${result.num2} = ${result.num1 * result.num2}`);
+            break;
+        case "%":
+            console.log(`Modulus of ${result.num1} % ${result.num2} = ${result.num1 % result.num2}`);
+            break;
+        case "/":
+            console.log(`Division of ${result.num1} / ${result.num2} = ${result.num1 / result.num2}`);
+            break;
+        case "^":
+            console.log(`Exponent of ${result.num1} ^ ${result.num2} = ${result.num1 ** result.num2}`);
+            break;
+        default:
+            break;
+    }
 }
-userInput();
-console.log("After executing the function.");
+calculator();
+//console.log("After executing the function.");
--- class11/index.ts
+++ class11/index.ts
@@ -1,4 +1,49 @@
-import inquirer, { Answers, QuestionCollection } from "inquirer";
+import inquirer, { QuestionCollection, Answers } from "inquirer";
+
+async function calculator() {
+  const questions: QuestionCollection = [{
+    name: "num1",
+    type: "number",
+    message: "Enter first number:"
+  },
+  {
+    name: "num2",
+    type: "number",
+    message: "Enter second number:"
+  },
+  {
+    name: "operation",
+    type: "list",
+    message: "Enter the operation:",
+    choices: ["+", "-", "*", "%", "/", "^"]
+  }
+  ]
+  const result: Answers = await inquirer.prompt(questions)
+  switch (result.operation) {
+    case "+":
+      console.log(`Sum of ${result.num1} + ${result.num2} = ${result.num1 + result.num2}`);
+      break;
+    case "-":
+      console.log(`Subtarction of ${result.num1} - ${result.num2} = ${result.num1 - result.num2}`);
+      break;
+    case "*":
+      console.log(`Multiplication of ${result.num1} * ${result.num2} = ${result.num1 * result.num2}`);
+      break;
+    case "%":
+      console.log(`Modulus of ${result.num1} % ${result.num2} = ${result.num1 % result.num2}`);
+      break;
+    case "/":
+      console.log(`Division of ${result.num1} / ${result.num2} = ${result.num1 / result.num2}`);
+      break;
+    case "^":
+      console.log(`Exponent of ${result.num1} ^ ${result.num2} = ${result.num1 ** result.num2}`);
+      break;
+    default:
+      break;
+  }
+}
+calculator()
+/*
 async function userInput() {
   const questions: QuestionCollection = [
     {
@@ -12,8 +57,10 @@ async function userInput() {
       message: "Enter the second number:",
     }
   ]
-  const result: Answers =  await inquirer.prompt(questions)
+  const result: results =  await inquirer.prompt(questions)
   console.log("Result of sum is:", result.num1 + result.num2);
 }
 await userInput()
 console.log("After executing the function.");
+*/
+
