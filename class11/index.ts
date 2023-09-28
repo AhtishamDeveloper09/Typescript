7b08735350c053330551221296bcfc76621e9355
AhtishamDeveloper09 <Ahtishamprine@gmail.com>
2023-09-28 23:57:09 +0500

    class11 file updated

--- class11/index.ts
+++ class11/index.ts
@@ -18,28 +18,33 @@ async function calculator() {
     choices: ["+", "-", "*", "%", "/", "^"]
   }
   ]
-  const result: Answers = await inquirer.prompt(questions)
-  switch (result.operation) {
-    case "+":
-      console.log(`Sum of ${result.num1} + ${result.num2} = ${result.num1 + result.num2}`);
-      break;
-    case "-":
-      console.log(`Subtarction of ${result.num1} - ${result.num2} = ${result.num1 - result.num2}`);
-      break;
-    case "*":
-      console.log(`Multiplication of ${result.num1} * ${result.num2} = ${result.num1 * result.num2}`);
-      break;
-    case "%":
-      console.log(`Modulus of ${result.num1} % ${result.num2} = ${result.num1 % result.num2}`);
-      break;
-    case "/":
-      console.log(`Division of ${result.num1} / ${result.num2} = ${result.num1 / result.num2}`);
-      break;
-    case "^":
-      console.log(`Exponent of ${result.num1} ^ ${result.num2} = ${result.num1 ** result.num2}`);
-      break;
-    default:
-      break;
+  try {
+    const result: Answers = await inquirer.prompt(questions)
+    switch (result.operation) {
+      case "+":
+        console.log(`Sum of ${result.num1} + ${result.num2} = ${result.num1 + result.num2}`);
+        break;
+      case "-":
+        console.log(`Subtarction of ${result.num1} - ${result.num2} = ${result.num1 - result.num2}`);
+        break;
+      case "*":
+        console.log(`Multiplication of ${result.num1} * ${result.num2} = ${result.num1 * result.num2}`);
+        break;
+      case "%":
+        console.log(`Modulus of ${result.num1} % ${result.num2} = ${result.num1 % result.num2}`);
+        break;
+      case "/":
+        console.log(`Division of ${result.num1} / ${result.num2} = ${result.num1 / result.num2}`);
+        break;
+      case "^":
+        console.log(`Exponent of ${result.num1} ^ ${result.num2} = ${result.num1 ** result.num2}`);
+        break;
+      default:
+        break;
+    }
+  }
+  catch (error) {
+    console.log("Something went wrong!", error);
   }
 }
 calculator()
